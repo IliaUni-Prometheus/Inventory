cdcfe7540c1a4224e3f0d769f2e01f8474fcc05a
Zako <zniqabadze@gmail.com>
2022-05-31 16:36:46 +0400

    first components

---
+++ ClientSide/Data/Implementations/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using Flurl;
+using Flurl.Http;
+
+namespace ClientSide.Data.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        public async Task<List<object>> All()
+        {
+            var res = await "https://localhost:7045/api/Employee".GetJsonAsync<object>();
+
+
+            return null;
+        }
+
+        public Task Delete(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> OfId(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClientSide/wwwroot/scripts/interop.js
@@ -0,0 +1,15 @@
+﻿function messageBox(message) {
+    window.alert(message);
+}
+
+function setColorInStorage(color) {
+    if (typeof (Storage) !== "undefined") {
+        localStorage.setItem("Color", color)
+    }
+}
+
+function getColorFromStorage() {
+    if (typeof (Storage) !== "undefined") {
+        return localStorage.getItem("Color")
+    }
+}
