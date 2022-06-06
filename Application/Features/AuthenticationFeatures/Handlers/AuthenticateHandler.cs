ac9c66f3b8d269847d639a6bede890a0fe9c7f0e
Garkeon <dragonforwar@gmail.com>
2022-06-06 22:48:17 +0400

    changed authentication to cqrs

---
+++ Application/Features/AuthenticationFeatures/Handlers/AuthenticateHandler.cs
@@ -0,0 +1,19 @@
+﻿using Application.DataContracts.Responses;
+using Application.Features.AuthenticationFeatures.Commands;
+using Application.Services.Abstract;
+using static Shared.CQRSInfrastructure;
+
+namespace Application.Features.AuthenticationFeatures.Handlers
+{
+    public class AuthenticateHandler : ICommandHandler<AuthenticateCommand, AuthenticateResponse>
+    {
+        private IUserService _userService;
+
+        public AuthenticateHandler(IUserService userService) { this._userService = userService; }
+
+        public async Task<AuthenticateResponse> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
+        {
+            return await _userService.AuthenticateAsync(request.UserModel);
+        }
+    }
+}
